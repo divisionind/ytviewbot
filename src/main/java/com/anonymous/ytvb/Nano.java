5d167c8608a42192c185f8388f5c17037de61b85
drew6017 <drew6017@gmail.com>
2019-05-25 09:14:48 -0500

    license updates

--- src/main/java/com/anonymous/ytvb/Nano.java
+++ src/main/java/com/anonymous/ytvb/Nano.java
@@ -1,10 +1,19 @@
 /*
- * Copyright (c) 2002-2017, the original author or authors.
+ * ytviewbot - just a YouTube view bot
+ * Copyright (C) 2019 Anonymous
  *
- * This software is distributable under the BSD license. See the terms of the
- * BSD license in the documentation provided with this software.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * https://opensource.org/licenses/BSD-3-Clause
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 package com.anonymous.ytvb;
 
