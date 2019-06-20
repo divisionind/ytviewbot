b2b2242bc52d33328cb22b348056b5fa8260041c
drew6017 <drew6017@gmail.com>
2019-06-20 05:47:40 -0500

    install files from release now by default have execute permissions

--- buildTar.sh
+++ buildTar.sh
@@ -3,4 +3,6 @@ mkdir ytviewbot
 cp libs/ytviewbot ytviewbot/ytviewbot
 cp ../installPackaged ytviewbot/install
 cp ../installDepends ytviewbot/installDepends
+chmod +x ytviewbot/install
+chmod +x ytviewbot/installDepends
 tar -czvf ytviewbot-$1.tar.gz ytviewbot/
